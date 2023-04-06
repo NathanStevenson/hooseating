229b50fbd5e69bf0cd9921657fb550c0592dc0eb
Fardeen Firoz Khan <ffk2002@gmail.com>
2023-04-06 17:37:45 -0400

    login1

--- form.php
+++ form.php
@@ -14,9 +14,7 @@
   <meta name="description" content="include some description about your page">  
     
   <title>Bootstrap example</title>
-  
-  bootstrap -->  
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="icon" type="image/png" href="http://www.cs.virginia.edu/~up3f/cs4750/images/db-icon.png" />
   
 
