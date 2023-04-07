7ea04152380a339d869b01d63a99e2cc059ef788
Nathan <nts7bcj@virginia.edu>
2023-04-07 11:54:57 -0400

    add user login functionality

--- login_register.php
+++ login_register.php
@@ -2,7 +2,6 @@
     // Function that sees whether or not a username has already been taken
     function username_taken($username){
         global $db;
-        debug_to_console($username);
         $query = "SELECT * FROM User where name=:username";
         $statement = $db->prepare($query);
         $statement->bindValue(':username', $username);
@@ -23,5 +22,29 @@
         $result = $statement->fetch();
         $statement->closeCursor();
         return $result;
+    }
+
+    // Function that registers a user in our database on login
+    function add_user($id, $uname, $pwd){
+        global $db;
+        $query = "INSERT INTO User VALUES (:id, :uname, NULL, :pwd);";
+        $statement = $db->prepare($query);
+        // bind the templates if not an executable
+        $statement->bindValue(':id', $id);
+        $statement->bindValue(':uname', $uname);
+        $statement->bindValue(':pwd', $pwd);
+        $statement->execute();
+        $statement->closeCursor();
+    }
+
+    // Finds the first available user_id
+    function max_user_id(){
+        global $db;
+        $query = "SELECT max(user_id) FROM User";
+        $statement = $db->prepare($query);
+        $statement->execute();
+        $results = $statement->fetchColumn();
+        $statement->closeCursor();
+        return $results;
     }
 ?>
