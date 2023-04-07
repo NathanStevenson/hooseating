efa2b7fcf6a641bfef2adb45e0240b71406f8721
Nathan <nts7bcj@virginia.edu>
2023-04-07 00:03:02 -0400

    stylized login page

--- form.php
+++ form.php
@@ -15,19 +15,25 @@
             $auth = checkLogin($_POST['name'], $_POST['password']);
             $authAttempt = 1;
             if($authAttempt){
+                // If the user is authenticated
                 if($auth){
-                //TODO: redirect
-                echo "CORRECT USERNAME AND PASSWORD";
+                    // Redirect the User into the main page
+                    $wrong_credential_msg = "";
+                // User entered the wrong info
                 }else{
-                echo "INCORRECT USERNAME OR PASSWORD";
+                    $wrong_credential_msg = "Invalid Credentials!";
                 }
             }
         }
         //  Code that checks whether or not the username currently exists when the user clicks the register button on the login page
         if (!empty($_POST['actionBtn']) && $_POST['actionBtn']=="REGISTER"){
             $res = username_taken($_POST['name']);
+            // If there is another user in the database with that name
             if ($res){
                 $username_exists_msg = "That username already exists!";
+            //  otherwise clear that msg to the user
+            }else{
+                $username_exists_msg = "";
             }
         }
     }
@@ -41,26 +47,34 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="author" content="your name">
         <meta name="description" content="include some description about your page">     
-        <title>Bootstrap example</title> 
+        <title>Hoos Eating</title> 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="icon" type="image/png" href="http://www.cs.virginia.edu/~up3f/cs4750/images/db-icon.png" />
     </head>
 
-    <body>
+    <!-- Should align the login box in the middle of the page vertically and mx-auto does horizontally -->
+    <body style="background-color:lightskyblue">
         <!-- User Login Form -->
-        <div class="container">
-            <form name="mainForm" action="form.php" method="post"> 
-                <div class="row mb-3 mx-3">
+        <div class="shadow rounded-3 mx-auto w-25" style="background-color:white; margin-top:10%;">
+            <h2 class="mt-3 pt-4 text-center">Hoos Eating Login</h2>
+            <form name="mainForm" action="form.php" method="post" class="mt-5 px-2"> 
+                <div class="row mb-3 mx-3" style="color:lightskyblue">
                     Username:
                     <input type="text" class="form-control" name="name" required />    
                 </div>  
-                <div class="row mb-3 mx-3">
+                <div class="row mb-3 mx-3" style="color:lightskyblue">
                     Password:
                     <input type="password" class="form-control" name="password" required />        
                 </div>  
+                <div class="mt-5 pb-4 mx-auto w-75 d-flex justify-content-evenly">
+                    <input type="submit" class="btn btn-primary" value="LOGIN" name="actionBtn" title="submit">
+                    <input type="submit" class="btn btn-primary" value="REGISTER" name="actionBtn" title="submit">
+                </div>
 
-                <input type="submit" class="btn btn-primary" value="LOGIN" name="actionBtn" title="submit">
-                <input type="submit" class="btn btn-primary" value="REGISTER" name="actionBtn" title="submit">
+                <p class="text-center fw-bold pb-3" style="color:red">
+                    <?php if($wrong_credential_msg != "") echo $wrong_credential_msg; ?>
+                    <?php if($username_exists_msg != "") echo $username_exists_msg; ?>   
+                </p> 
             </form>     
         </div>    
     </body>
